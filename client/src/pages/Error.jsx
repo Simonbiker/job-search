003839cd48d74b65e949dd4003af51e7f822b69e
Simon Mac Aree <smacr25@gmail.com>
2023-10-10 10:52:44 +0100

    Styling error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,28 @@
 import {Link, useRouteError} from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const Error = () => {
     const error = useRouteError();
     
-    console.log(error);
+    if(error.status === 404) {
+        return <Wrapper>
+            <div>
+                <img src={img} alt='not found'/>
+                <h3>Ohh! page not found</h3>
+                <p>We can't seem to find the page you're looking for</p>
+                <Link to='/dashboard'>back home</Link>
+            </div>
+        </Wrapper>
+    }
+
     return (
-        <div>
-            <h1>Error</h1>
-            <Link to='/'>Back home</Link>
-        </div>
+        <Wrapper>
+            <div>
+                <h3>Somthing went wrong</h3>
+            </div>  
+        </Wrapper>
+        
     )
 };
 
